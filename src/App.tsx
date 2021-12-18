fdf293718cc3bb1ddde0146bdeceee92597d10fa
Raf Sz <raf.sz@icloud.com>
2021-12-18 16:02:02 +0000

    Make eslint & prettier work and add a few components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import * as React from 'react';
+import './css/app.scss';
+import {ErrorMessage} from '~/components/ErrorMessage';
 
 const App: React.FC = () => (
   <div>
     <h1>React 17 and TypeScript 4 App!🚀</h1>
+    <ErrorMessage error="This is a test error!" />
   </div>
 );
 
+// eslint-disable-next-line import/no-default-export
 export default App;
---
+++ src/types/BootstrapSize.ts
@@ -0,0 +1,1 @@
+export type BootstrapSize = 'sm' | 'lg';
